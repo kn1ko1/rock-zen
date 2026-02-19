cb82770d557a4f1be0fa0e701b0bec033cec6a4e
kn1ko1 <niquaye@gmail.com>
2026-02-19 19:04:45 +0000

    Add PWA support: manifest, service worker, and mobile meta tags

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'arcade-v1';
+const urlsToCache = [
+  '/rock-zen/',
+  '/rock-zen/index.html',
+  '/rock-zen/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+      .catch(() => {
+        if (event.request.destination === 'document') {
+          return caches.match('/rock-zen/');
+        }
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
